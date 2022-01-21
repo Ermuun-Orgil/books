3242a666ff991a439f7526b48fcf50c933dcd61c
IceCream <ermuunee10@gmail.com>
2022-01-21 14:07:29 +0800

    ok

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+const mongoose = require('mongoose');
 
 function App() {
+  // mongoose.connect(
+  //   "mongodb+srv://ermuun:animemanga10@cluster0.8befs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+  // );
+  mongoose.connect('mongodb+srv://ermuun:animemanga10@cluster0.8befs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {useNewUrlParser: true});
+
+  const Books = mongoose.model("books", { title: String });
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>ok</div>
+      <div>ok</div>
+      <div>ok</div>
+    </>
   );
 }
 
